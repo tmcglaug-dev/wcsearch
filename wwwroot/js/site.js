72584619edc5a5e9eb08df92b8bdf0ca791e01a7
tmcglaug-dev <59187579+tmcglaug-dev@users.noreply.github.com>
2019-12-23 22:33:11 -0500

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var checkbox = document.getElementById("fc");
+var showfield = document.getElementById("fields");
+
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+
+        showfield.style.display = "inline";
+        // Checkbox is checked..
+    } else {
+
+        showfield.style.display = "none";
+        // Checkbox is not checked..
+    }
+});
